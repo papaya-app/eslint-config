20df9ac7759e9ed9abc9d5e40dec63a74f910685
fengjianhao <lingj.jhf@outlook.com>
2024-03-21 10:25:39 +0800

    修改ts配置

--- ts.js
+++ ts.js
@@ -13,11 +13,5 @@ module.exports = {
   rules: {
     ...customized.rules,
   },
-  extends: require.resolve('./rules/javascript'),
-  overrides: [
-    {
-      files: ['*.ts'],
-      extends: [require.resolve('./rules/typescript'), require.resolve('./rules/import')],
-    },
-  ],
+  extends: [require.resolve('./rules/typescript'), require.resolve('./rules/import')],
 }
